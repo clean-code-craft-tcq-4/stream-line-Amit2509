1d1ea7ca272d48779f0f1cc1465b8d6d4a7331ca
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-10-27 13:21:15 +0530

    Update BMS_Receiver.c

--- Receiver/BMS_Receiver.c
+++ Receiver/BMS_Receiver.c
@@ -11,3 +11,14 @@ void ReadFromConsoleInput(float* temperature, float* SOC)
 {
   ReadParameters(temperature, SOC);
 }
+
+void ReadConsolePrintMinMaxSMA()
+{
+  float temperature, SOC, ChargeRate;
+  char title[30];
+  fgets(title,30,stdin);
+  for (int i=0; i<50; i++)
+  {
+    ReadFromConsoleInput(&temperature, &SOC);
+  
+}
