4c64c088af98d9fc3725f43d389b58a678edfda6
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-10-31 17:37:14 +0530

    Update BMS_Receiver.c

--- Receiver/BMS_Receiver.c
+++ Receiver/BMS_Receiver.c
@@ -20,14 +20,12 @@ int ReadConsole(float* temperature, float* SOC)
 void ReadParameters(float* SOC,float* temperature)
 {
   ReadConsole(temperature, SOC);
-  
+  printf(temperature1=%f,SOC1=%f\n,temperature,SOC);
   /*Shifting the elements in the array for Moving Average calculation*/
    for(int prmsloop = 0; prmsloop<NUMBER_OF_SAMPLES_FOR_SMA-1; prmsloop++)
   {
     Temp_Array[prmsloop] = Temp_Array[prmsloop+1];
-     printf("Temp_Array=%f\n",Temp_Array[prmsloop]);
     SOC_Array[prmsloop] = SOC_Array[prmsloop+1];
-     printf("SOC_Array=%f\n",Temp_Array[prmsloop]);
   }
   
   /*Inserting the latest data into the last element of array*/
