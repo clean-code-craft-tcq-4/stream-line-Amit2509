14ee1504d9b62ebd0ee53a342113f100fe2a1e4e
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-10-31 15:58:36 +0530

    Update BMS_Receiver.c

--- Receiver/BMS_Receiver.c
+++ Receiver/BMS_Receiver.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "BMS_Receiver.h"
 
-extern float Min_Temp;
-extern float Max_Temp;
-extern float SMA_Temp;
-extern float Temp_Array[NUMBER_OF_SAMPLES_FOR_SMA];
-extern float Min_SOC;
-extern float Max_SOC;
-extern float SMA_SOC;
-extern float SOC_Array[NUMBER_OF_SAMPLES_FOR_SMA];
-extern float Min_ChargeRate;
-extern float Max_ChargeRate;
-extern float SMA_ChargeRate;
-extern float ChargeRate_Array[NUMBER_OF_SAMPLES_FOR_SMA];
+float Min_Temp = 0;
+float Max_Temp = 0;
+float SMA_Temp = 0;
+float Temp_Array[NUMBER_OF_SAMPLES_FOR_SMA] = {0};
+float Min_SOC = 0;
+float Max_SOC = 0;
+float SMA_SOC = 0;
+float SOC_Array[NUMBER_OF_SAMPLES_FOR_SMA] = {0};
+float Min_ChargeRate = 0;
+float Max_ChargeRate = 0;
+float SMA_ChargeRate = 0;
+float ChargeRate_Array[NUMBER_OF_SAMPLES_FOR_SMA] = {0};
 
 
 int ReadParameters(float* temperature, float* SOC)
