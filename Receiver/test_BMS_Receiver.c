be5e387ace2362c403e70bff2114227ca24328dc
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-10-31 12:25:10 +0530

    Update test_BMS_Receiver.c

--- Receiver/test_BMS_Receiver.c
+++ Receiver/test_BMS_Receiver.c
@@ -35,38 +35,37 @@ void testFindSMA()
   {
     Temp_Array[i] = i;
     SOC_Array[i] = 1.5*i;
-    ChargeRate_Array[i] = (float)i/NUMBER_OF_SAMPLES_FOR_SMA;
+    
   }
   
   float Temp_Sum = 0, Temp_Avg;
   float SOC_Sum = 0, SOC_Avg;
-  float ChargeRate_Sum = 0, ChargeRate_Avg;
+  
   for (int i = 0; i < NUMBER_OF_SAMPLES_FOR_SMA; i++)
   {
       Temp_Sum +=Temp_Array[i];
       SOC_Sum +=SOC_Array[i];
-      ChargeRate_Sum +=ChargeRate_Array[i];
+
   }
   
   Temp_Avg = Temp_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
   SOC_Avg = SOC_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
-  ChargeRate_Avg = ChargeRate_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
   FindSMA();
   
   assert(SMA_Temp == Temp_Avg);
   assert(SMA_SOC = SOC_Avg);
-  assert(SMA_ChargeRate == ChargeRate_Avg);
+  
   
   /*Reset all Global Variables to zero after the test case*/
   for (int i=0; i<NUMBER_OF_SAMPLES_FOR_SMA; i++)
   {
     Temp_Array[i] = 0;
     SOC_Array[i] = 0;
-    ChargeRate_Array[i] = 0;
+    
   }
   SMA_Temp = 0;
   SMA_SOC = 0;
-  SMA_ChargeRate = 0;  
+  
 }
 
 void testPrintMinMaxSMA()
