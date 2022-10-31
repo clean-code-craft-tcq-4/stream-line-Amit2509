e31dd956f1e23ad0096928b1b4cfcffcd29cca2e
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-10-31 11:17:26 +0530

    Update BMS_Receiver.c

--- Receiver/BMS_Receiver.c
+++ Receiver/BMS_Receiver.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include "BMS_Receiver.h"
+float Min_Temp = 0;
+float Max_Temp = 0;
+float SMA_Temp = 0;
+float Temp_Array[NUMBER_OF_SAMPLES_FOR_SMA] = {0};
+float Min_SOC = 0;
+float Max_SOC = 0;
+float SMA_SOC = 0;
+float SOC_Array[NUMBER_OF_SAMPLES_FOR_SMA] = {0};
+float Min_ChargeRate = 0;
+float Max_ChargeRate = 0;
+float SMA_ChargeRate = 0;
+float ChargeRate_Array[NUMBER_OF_SAMPLES_FOR_SMA] = {0};
+
+
 
 void ReadParameters(float* temperature, float* SOC)
 {
   char dummy;
-  scanf("%f%c%f", temperature, &dummy, SOC);
+  return scanf("%f%c%f", temperature, &dummy, SOC);
   
   printf("Temp:%f,SOC:%f\n",temperature,SOC);
 }
@@ -12,6 +26,61 @@ void ReadParameters(float* temperature, float* SOC)
 void ReadFromConsoleInput(float* temperature, float* SOC)
 {
   ReadParameters(temperature, SOC);
+  
+   for(int i = 0; i<NUMBER_OF_SAMPLES_FOR_SMA-1; i++)
+  {
+    Temp_Array[i] = Temp_Array[i+1];
+    SOC_Array[i] = SOC_Array[i+1];
+    ChargeRate_Array[i] = ChargeRate_Array[i+1];
+  }
+  
+  /*Inserting the latest data into the last element of array*/
+  Temp_Array[NUMBER_OF_SAMPLES_FOR_SMA-1] = *temperature;
+  SOC_Array[NUMBER_OF_SAMPLES_FOR_SMA-1] = *SOC;
+  ChargeRate_Array[NUMBER_OF_SAMPLES_FOR_SMA-1] = *ChargeRate;
+    
+  return;
+}
+
+void FindMinAndMax(float data, float* Min, float* Max)
+{
+   if (data < *Min)
+   {
+     *Min = data;
+   }
+   else if (data > *Max)
+   {
+     *Max = data;
+   }
+   else
+   {
+     /*Do nothing*/
+   }
+}
+
+void FindSMA ()
+{
+  float Temp_Sum = 0;
+  float SOC_Sum = 0;
+  float ChargeRate_Sum = 0;
+    for (int i = 0; i < NUMBER_OF_SAMPLES_FOR_SMA; i++)
+    {
+      Temp_Sum +=Temp_Array[i];
+      SOC_Sum +=SOC_Array[i];
+      ChargeRate_Sum +=ChargeRate_Array[i];
+    }
+  SMA_Temp = Temp_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
+  SMA_SOC = SOC_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
+  SMA_ChargeRate = ChargeRate_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
+}
+
+int PrintMinMaxSMA()
+{
+    printf("Temperature: Min = %f, Max = %f, SMA = %f\n", Min_Temp, Max_Temp, SMA_Temp);
+    printf("SOC: Min = %f, Max = %f, SMA = %f\n", Min_SOC, Max_SOC, SMA_SOC);
+    printf("ChargeRate: Min = %f, Max = %f, SMA = %f\n\n", Min_ChargeRate, Max_ChargeRate, SMA_ChargeRate);
+  
+  return 1;
 }
 
 void ReadConsolePrintMinMaxSMA()
@@ -22,6 +91,11 @@ void ReadConsolePrintMinMaxSMA()
   for (int i=0; i<50; i++)
   {
     ReadFromConsoleInput(&temperature, &SOC);
+    FindMinAndMax(temperature, &Min_Temp, &Max_Temp);
+    FindMinAndMax(SOC, &Min_SOC, &Max_SOC);
+    FindMinAndMax(ChargeRate, &Min_ChargeRate, &Max_ChargeRate);
+    FindSMA();
+    PrintMinMaxSMA();
   }
   
 }
