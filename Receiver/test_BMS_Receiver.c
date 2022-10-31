da00452207c062132b72e0c6a8b61ab7106643e9
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-10-31 14:46:08 +0530

    Update test_BMS_Receiver.c

--- Receiver/test_BMS_Receiver.c
+++ Receiver/test_BMS_Receiver.c
@@ -3,6 +3,35 @@
 #include "BMS_Receiver.h"
 
 
+void testFindMinAndMax()
+{
+  /*Test Case: 1*/
+  float data = 3;
+  float Min = 5;
+  float Max = 7;
+  FindMinAndMax(data, &Min, &Max);
+  assert(Min == 3);
+  assert(Max == 7);
+  
+  /*Test Case: 2*/
+  data = 6;
+  Min = 5;
+  Max = 7;
+  FindMinAndMax(data, &Min, &Max);
+  assert(Min == 5);
+  assert(Max == 7); 
+  
+  /*Test Case: 3*/
+  data = 8;
+  Min = 5;
+  Max = 7;
+  FindMinAndMax(data, &Min, &Max);
+  assert(Min == 5);
+  assert(Max == 8); 
+}
+
+
+
 
 int main()
 {
@@ -10,7 +39,7 @@ int main()
   ReadConsolePrintMinMaxSMA(); 
   
   
- // testFindSMA();
+  testFindSMA();
   
  // testPrintMinMaxSMA();
   
